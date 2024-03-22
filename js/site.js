e30666bd7aa8c1d67214e938209187df406bbfd8
Tcaldwell22 <143471802+Tcaldwell22@users.noreply.github.com>
2024-03-21 22:22:39 -0500

    Created JS Validation

--- js/site.js
+++ js/site.js
@@ -2,5 +2,11 @@
 jQuery(function($) {
   $('html').removeClass('nojs');
   $("html").addClass("hasjs");
- 
+  $("#submit").on("click", function () {
+    let inputString = document.forms.appointment.fname.value
+    let firstUppercase = /^[A-Z]/.test(inputString);
+    if (!firstUppercase) {
+      alert("Name should start with an uppercase letter.");
+    }
+  });
 });
